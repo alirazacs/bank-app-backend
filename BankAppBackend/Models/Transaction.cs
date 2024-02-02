1a1525652e43d2ae786093cf92e5cff83442c7ff
ali raza <ali.raza@uworx.co.uk>
2024-02-02 10:32:32 +0500

    Added Transaction model

---
+++ BankAppBackend/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿namespace BankAppBackend.Models
+{
+    public enum TransactionType
+    {
+        CREDIT,DEBIT,TRANSFER
+    }
+    public class Transaction
+    {
+        public long Id { get; set; }
+
+        public long TransactionId { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public double Amount { get; set; }
+        public DateTime DateTime { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; } 
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
+
+
+    }
+}
