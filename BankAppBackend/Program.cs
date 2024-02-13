3eccefcaf98912c416e3a296c019d0a803d79fe9
Waqas Ahmed Rehmani <waqasahmed.rehmani@uworx.co.uk>
2024-02-13 16:02:05 +0500

    apply migrations on startup

--- BankAppBackend/Program.cs
+++ BankAppBackend/Program.cs
@@ -16,8 +16,6 @@ builder.Services.AddControllersWithViews()
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-string connectionString = builder.Configuration.GetConnectionString("SQLConnectionString") ?? throw new InvalidOperationException("Connection string of name SQLConnectionString not found");
-builder.Services.AddDbContext<DatabaseContext>(conn => conn.UseSqlServer(connectionString));
 builder.Services.AddScoped<IApplicantRepository, ApplicantRepository>();
 builder.Services.AddTransient<ITellerRepository, TellerRepository>();
 builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
@@ -29,8 +27,14 @@ builder.Services.AddScoped<IAccountService, AccountService>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 builder.Services.AddScoped<ITransactionService, TransactionService>();
 builder.Services.AddScoped<IRedisMessagePublisherService, RedisMessagePublisherService>();
-var app = builder.Build();
 
+string connectionString = builder.Configuration.GetConnectionString("SQLConnectionString") ?? throw new InvalidOperationException("Connection string of name SQLConnectionString not found");
+builder.Services.AddDbContext<DatabaseContext>(conn => conn.UseSqlServer(connectionString));
+// Apply migrations
+var app = builder.Build();
+using var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+await context.Database.MigrateAsync();
 
 //Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -45,4 +49,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
