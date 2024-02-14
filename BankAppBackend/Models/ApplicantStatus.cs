ae8264cf23f8622f72a2783dab239be777e94847
Alamgir Hassan <alamgir.hassan@uworx.co.uk>
2024-02-14 12:30:44 +0500

    Deleted everything in develop branch

--- BankAppBackend/Models/ApplicantStatus.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BankAppBackend.Models
-{
-    public class ApplicantStatus
-    {
-        public AccountStatus AccountStatus { get; set; }    
-        public long TellerId { get; set; }  
-    }
-}
